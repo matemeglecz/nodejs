a119689cd1704201caf1bb99d832a70aa4a8ec65
Máté Meglécz <meglecz.mate@gmail.com>
2021-09-28 23:14:47 +0200

    index.js fix

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
    
 const express = require('express');
 const app = express();
-//app.use("/", function(req,res){res.send("Mate")})
 app.use(express.static('static'));
 app.listen(3000, function () {
     console.log('Hello :3000');
